d1cef9a6202c2aae357f14a3f1d959caf60f1919
Sophie D <sophie.delaux@outlook.fr>
2016-11-06 10:39:34 +0100

    Event manager with sample data

---
+++ IO_serialization/event_manager/event_manager.rb
@@ -0,0 +1,67 @@
+require "csv"
+require "sunlight/congress"
+require "erb"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+#Read entire file
+#contents = File.read "event_attendees.csv"
+#puts contents
+
+#Read line by line
+#lines = File.readlines "event_attendees.csv"
+#lines.each do |line|
+#    puts line
+#end
+
+#Format the zipcode from the file
+def clean_zipcode(zipcode)
+    if zipcode.nil?
+        zipcode = "00000"
+    elsif zipcode.length < 5
+        zipcode = zipcode.rjust 5, "0"
+    elsif zipcode.length > 5
+        zipcode = zipcode[0..4]
+    else
+        zipcode
+    end
+end
+
+#Get legislators for each zipcde
+def legislators_by_zipcode(zipcode)
+
+     Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+#Save HTML letters in an "ouput" directory
+def save_thank_you_letters(id, form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+    
+    filename = "output/thanks_#{id}.html"
+    
+    File.open(filename, 'w') do |file|
+        file.puts form_letter
+    end
+    
+end
+
+puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.html"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+    id = row[0]
+
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+
+    legislators = legislators_by_zipcode(zipcode)
+    
+    form_letter = erb_template.result(binding)
+    
+    save_thank_you_letters(id, form_letter)
+end
+
